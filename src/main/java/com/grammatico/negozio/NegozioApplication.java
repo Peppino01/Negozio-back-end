b7a2fdcf147c7294130604015f00db8fffcf4852
Giuseppe Grammatico <giu17grammatico@gmail.com>
2023-06-22 18:26:59 +0200

    first commit

---
+++ src/main/java/com/grammatico/negozio/NegozioApplication.java
@@ -0,0 +1,13 @@
+package com.grammatico.negozio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NegozioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NegozioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grammatico/negozio/NegozioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grammatico.negozio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NegozioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
