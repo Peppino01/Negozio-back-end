8ac9c04dbd70f6dd522b827bd8c0b20685906d8e
Giuseppe Grammatico <giu17grammatico@gmail.com>
2023-06-29 12:27:03 +0200

    fixed little bugs

--- src/main/java/com/grammatico/negozio/DTO/inputDTO/ClienteInputDTO.java
+++ src/main/java/com/grammatico/negozio/DTO/inputDTO/ClienteInputDTO.java
@@ -1,23 +1,17 @@
 package com.grammatico.negozio.DTO.inputDTO;
 
 import java.sql.Date;
-import java.util.List;
 
 import com.grammatico.negozio.model.Genere;
-import com.grammatico.negozio.model.entity.Recenzione;
-import com.grammatico.negozio.model.entity.Transazione;
 
 public record ClienteInputDTO (
-    Long id,
     String nome,
     String cognome,
     String email,
     String password,
     String numTelefono,
     Date dataNascita,
-    Genere genere,
-    List<Transazione> transazioni,
-    List<Recenzione> recenzioni
+    Genere genere
 ) {
     
 }
--- src/main/java/com/grammatico/negozio/DTO/inputDTO/DipendenteInputDTO.java
+++ src/main/java/com/grammatico/negozio/DTO/inputDTO/DipendenteInputDTO.java
@@ -3,7 +3,6 @@ package com.grammatico.negozio.DTO.inputDTO;
 import java.sql.Date;
 
 public record DipendenteInputDTO (
-    Long id,
     String nome,
     String cognome,
     String email,
