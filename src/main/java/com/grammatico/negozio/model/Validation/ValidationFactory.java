f49de22f44aee3f92270bf876de0dcf4936d9956
Giuseppe Grammatico <giu17grammatico@gmail.com>
2023-09-05 21:13:13 +0200

    last commit

--- src/main/java/com/grammatico/negozio/model/Validation/ValidationFactory.java
+++ src/main/java/com/grammatico/negozio/model/Validation/ValidationFactory.java
@@ -4,6 +4,13 @@ import java.util.Date;
 
 import com.grammatico.negozio.model.Genere;
 
+/**
+ * La funzione di questa classe è quella di agevolare la creazione di due diverse tipologie di validazione:
+ * la prima, denominata LoginValidation, è finalizzata alla verifica delle credenziali per l'accesso, mentre la seconda,
+ * denominata SigninValidation, si concentra sulla convalida dei dati per il processo di registrazione. Questa struttura
+ * organizzativa permette una gestione più efficace e modulare delle diverse esigenze di validazione all'interno
+ * dell'applicazione.
+ */
 public class ValidationFactory {
     
     public Validation createLoginValidation(String email, String password) {
